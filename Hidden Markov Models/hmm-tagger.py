edef30a16aa4bd1cea857e6afcf9af02d4f2d58f
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-11-01 21:25:09 +0530

    Accuracy Function Implemented

--- Hidden Markov Models/hmm-tagger.py
+++ Hidden Markov Models/hmm-tagger.py
@@ -85,3 +85,38 @@ def simplify_decoding(X, model):
     """X should be a 1-D sequence of observations for the model to predict"""
     _, state_path = model.viterbi(replace_unknown(X))
     return [state[1].name for state in state_path[1:-1]]  # do not show the start/end state predictions
+
+def accuracy(X, Y, model):
+    """Calculate the prediction accuracy by using the model to decode each sequence
+    in the input X and comparing the prediction with the true labels in Y.
+    
+    The X should be an array whose first dimension is the number of sentences to test,
+    and each element of the array should be an iterable of the words in the sequence.
+    The arrays X and Y should have the exact same shape.
+    
+    X = [("See", "Spot", "run"), ("Run", "Spot", "run", "fast"), ...]
+    Y = [(), (), ...]
+    """
+    correct = total_predictions = 0
+    for observations, actual_tags in zip(X, Y):
+        
+        # The model.viterbi call in simplify_decoding will return None if the HMM
+        # raises an error (for example, if a test sentence contains a word that
+        # is out of vocabulary for the training set). Any exception counts the
+        # full sentence as an error (which makes this a conservative estimate).
+        try:
+            most_likely_tags = simplify_decoding(observations, model)
+            correct += sum(p == t for p, t in zip(most_likely_tags, actual_tags))
+        except:
+            pass
+        total_predictions += len(observations)
+    return correct / total_predictions
+
+mfc_training_acc = accuracy(data.training_set.X, data.training_set.Y, mfc_model)
+print("training accuracy mfc_model: {:.2f}%".format(100 * mfc_training_acc))
+
+mfc_testing_acc = accuracy(data.testing_set.X, data.testing_set.Y, mfc_model)
+print("testing accuracy mfc_model: {:.2f}%".format(100 * mfc_testing_acc))
+
+assert mfc_training_acc >= 0.955, "Uh oh. Your MFC accuracy on the training set doesn't look right."
+assert mfc_testing_acc >= 0.925, "Uh oh. Your MFC accuracy on the testing set doesn't look right."
