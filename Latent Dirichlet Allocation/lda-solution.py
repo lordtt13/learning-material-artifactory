f5377e491163446d3bbeb57fe1d8005eff464f3b
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-11-03 22:13:36 +0530

    Finished Implementation on BOW and TFIDF Corpus

--- Latent Dirichlet Allocation/lda-solution.py
+++ Latent Dirichlet Allocation/lda-solution.py
@@ -10,11 +10,11 @@ import gensim
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from gensim import corpora, models
 from nltk.stem.porter import *
 import nltk
 nltk.download('wordnet')
 
-np.random_seed(69422)
 
 data = pd.read_csv('abcnews-date-text.csv', error_bad_lines=False);
 # We only need the Headlines text column from the data
@@ -26,7 +26,7 @@ documents = data_text
 stemmer = SnowballStemmer("english")
 
 def lemmatize_stemming(text):
-    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v')) # Lemmatize text to present tense
 
 # Tokenize and lemmatize
 def preprocess(text):
@@ -39,6 +39,11 @@ def preprocess(text):
 
 processed_docs = documents['headline_text'].map(preprocess)
 
+# Bag of Words Implementation
+'''
+Create a dictionary from 'processed_docs' containing the number of times a word appears 
+in the training set using gensim.corpora.Dictionary and call it 'dictionary'
+'''
 dictionary = gensim.corpora.Dictionary(processed_docs)
 
 '''
@@ -50,3 +55,39 @@ Remove very rare and very common words:
 
 dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n=100000)
 
+'''
+Create the Bag-of-words model for each document i.e for each document we create a dictionary reporting how many
+words and how many times those words appear. Save this to 'bow_corpus'
+'''
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs] # (token_id, token_count)
+
+# TF-IDF Implementation
+'''
+Create tf-idf model object using models.TfidfModel on 'bow_corpus' and save it to 'tfidf'
+'''
+tfidf = models.TfidfModel(bow_corpus)
+
+# Apply Transform
+corpus_tfidf = tfidf[bow_corpus]
+
+lda_model = gensim.models.LdaMulticore(bow_corpus, 
+                                       num_topics=10, 
+                                       id2word = dictionary, 
+                                       passes = 2, 
+                                       workers=2)
+
+lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, 
+                                             num_topics=10, 
+                                             id2word = dictionary, 
+                                             passes = 2, 
+                                             workers=4)
+
+# Testing on Unseen Data
+
+unseen_document = "My favorite sports activities are running and swimming."
+
+# Data preprocessing step for the unseen document
+bow_vector = dictionary.doc2bow(preprocess(unseen_document))
+
+for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
+    print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))                                          
