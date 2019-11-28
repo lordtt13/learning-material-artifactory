e7785284b37a1e3983227577d973cdf44766d265
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-11-28 14:35:25 +0530

    Data processing added (tf)

---
+++ Sentiment Analysis/sentiment_rnn.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 14:31:08 2019
+
+@author: Tanmay Thakur
+"""
+
+import numpy as np
+import tensorflow as tf
+from string import punctuation
+from collections import Counter
+
+
+with open('../sentiment-network/reviews.txt', 'r') as f:
+    reviews = f.read()
+with open('../sentiment-network/labels.txt', 'r') as f:
+    labels = f.read()
+
+# Data Preprocessing    
+all_text = ''.join([c for c in reviews if c not in punctuation])
+reviews = all_text.split('\n')
+
+all_text = ' '.join(reviews)
+words = all_text.split()
+
+counts = Counter(words)
+vocab = sorted(counts, key=counts.get, reverse=True)
+vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
+
+reviews_ints = []
+for each in reviews:
+    reviews_ints.append([vocab_to_int[word] for word in each.split()])
+    
+
+labels = labels.split('\n')
+labels = np.array([1 if each == 'positive' else 0 for each in labels])
+
+review_lens = Counter([len(x) for x in reviews_ints])
+
+non_zero_idx = [ii for ii, review in enumerate(reviews_ints) if len(review) != 0]
+
+reviews_ints = [reviews_ints[ii] for ii in non_zero_idx]
+labels = np.array([labels[ii] for ii in non_zero_idx])
+
+seq_len = 200
+features = np.zeros((len(reviews_ints), seq_len), dtype=int)
+for i, row in enumerate(reviews_ints):
+    features[i, -len(row):] = np.array(row)[:seq_len]
+    
