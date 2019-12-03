b3d87790a1e72c2580ec6e77be30205ce1236b7b
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-03 13:47:14 +0530

    Reduced component visualization with tsne added

--- Sentiment Analysis/sentiment_classification.py
+++ Sentiment Analysis/sentiment_classification.py
@@ -9,6 +9,12 @@ from collections import Counter
 import time
 import sys
 import numpy as np
+import matplotlib.colors as colors
+from sklearn.manifold import TSNE
+
+from bokeh.models import ColumnDataSource, LabelSet
+from bokeh.plotting import figure, show, output_file
+
 
 
 def pretty_print_review_and_label(i):
@@ -822,3 +828,49 @@ def get_most_similar_words(focus = "horrible"):
     
     return most_similar.most_common()
 
+words_to_visualize = list()
+for word, ratio in pos_neg_ratios.most_common(500):
+    if(word in mlp_full.word2index.keys()):
+        words_to_visualize.append(word)
+    
+for word, ratio in list(reversed(pos_neg_ratios.most_common()))[0:500]:
+    if(word in mlp_full.word2index.keys()):
+        words_to_visualize.append(word)
+
+pos = 0
+neg = 0
+
+colors_list = list()
+vectors_list = list()
+for word in words_to_visualize:
+    if word in pos_neg_ratios.keys():
+        vectors_list.append(mlp_full.weights_0_1[mlp_full.word2index[word]])
+        if(pos_neg_ratios[word] > 0):
+            pos+=1
+            colors_list.append("#00ff00")
+        else:
+            neg+=1
+            colors_list.append("#000000")
+            
+tsne = TSNE(n_components=2, random_state=0)
+words_top_ted_tsne = tsne.fit_transform(vectors_list)
+
+p = figure(tools="pan,wheel_zoom,reset,save",
+           toolbar_location="above",
+           title="vector T-SNE for most polarized words")
+
+source = ColumnDataSource(data=dict(x1=words_top_ted_tsne[:,0],
+                                    x2=words_top_ted_tsne[:,1],
+                                    names=words_to_visualize,
+                                    color=colors_list))
+
+p.scatter(x="x1", y="x2", size=8, source=source, fill_color="color")
+
+word_labels = LabelSet(x="x1", y="x2", text="names", y_offset=6,
+                  text_font_size="8pt", text_color="#555555",
+                  source=source, text_align='center')
+p.add_layout(word_labels)
+
+show(p)
+
+# green indicates positive words, black indicates negative words
