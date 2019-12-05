f7c1372844e9e63b6cb8b4c706068a59aa7ff015
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-06 03:44:34 +0530

    Sentiment classification data mining done

---
+++ Sentiment Analysis/sentiment_classification.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 03:34:13 2019
+
+@author: Tanmay Thakur
+"""
+
+from collections import Counter
+import numpy as np
+
+
+def pretty_print_review_and_label(i):
+    print(labels[i] + "\t:\t" + reviews[i][:80] + "...")
+
+g = open('reviews.txt','r') # What we know!
+reviews = list(map(lambda x:x[:-1],g.readlines()))
+g.close()
+
+g = open('labels.txt','r') # What we WANT to know!
+labels = list(map(lambda x:x[:-1].upper(),g.readlines()))
+g.close()
+
+positive_counts = Counter()
+negative_counts = Counter()
+total_counts = Counter()
+
+for i in range(len(reviews)):
+    if(labels[i] == 'POSITIVE'):
+        for word in reviews[i].split(" "):
+            positive_counts[word] += 1
+            total_counts[word] += 1
+    else:
+        for word in reviews[i].split(" "):
+            negative_counts[word] += 1
+            total_counts[word] += 1
+            
+pos_neg_ratios = Counter()
+
+for term,cnt in list(total_counts.most_common()):
+    if(cnt > 100):
+        pos_neg_ratio = positive_counts[term] / float(negative_counts[term]+1)
+        pos_neg_ratios[term] = pos_neg_ratio
+        
+# Convert ratios to logs
+for word,ratio in pos_neg_ratios.most_common():
+    pos_neg_ratios[word] = np.log(ratio)
