ddd8633e94076f873b9dc5f5c2dc7d5aee402364
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-06 12:44:54 +0530

    More preprocessing for i/o

--- Sentiment Analysis/sentiment_classification.py
+++ Sentiment Analysis/sentiment_classification.py
@@ -44,3 +44,49 @@ for term,cnt in list(total_counts.most_common()):
 # Convert ratios to logs
 for word,ratio in pos_neg_ratios.most_common():
     pos_neg_ratios[word] = np.log(ratio)
+    
+vocab = set(total_counts.keys())
+vocab_size = len(vocab)
+
+layer_0 = np.zeros((1,vocab_size))
+
+
+# Create a dictionary of words in the vocabulary mapped to index positions 
+# (to be used in layer_0)
+word2index = {}
+for i,word in enumerate(vocab):
+    word2index[word] = i
+    
+def update_input_layer(review):
+    """ Modify the global layer_0 to represent the vector form of review.
+    The element at a given index of layer_0 should represent
+    how many times the given word occurs in the review.
+    Args:
+        review(string) - the string of the review
+    Returns:
+        None
+    """
+     
+    global layer_0
+    
+    # clear out previous state, reset the layer to be all 0s
+    layer_0 *= 0
+    
+    # count how many times each word is used in the given review and store the results in layer_0 
+    for word in review.split(" "):
+        layer_0[0][word2index[word]] += 1
+        
+update_input_layer(reviews[0])
+
+def get_target_for_label(label):
+    """Convert a label to `0` or `1`.
+    Args:
+        label(string) - Either "POSITIVE" or "NEGATIVE".
+    Returns:
+        `0` or `1`.
+    """
+    if(label == 'POSITIVE'):
+        return 1
+    else:
+        return 0
+    
