4501ac1599e46a29c7cda5d9d9a079e181c70bb8
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-14 03:05:21 +0530

    Custom Predicitions Function Added

--- Voice to Text/main.py
+++ Voice to Text/main.py
@@ -36,8 +36,11 @@ import _pickle as pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from data_generator import AudioGenerator, plot_raw_audio, plot_spectrogram_feature
+from utils import int_sequence_to_text
+
 from IPython.display import Markdown, display, Audio
-from data_generator import vis_train_features, plot_raw_audio, plot_spectrogram_feature, plot_mfcc_feature
+from data_generator import vis_train_features, plot_mfcc_feature
 from keras.backend.tensorflow_backend import set_session
 from keras.optimizers import SGD
 
@@ -487,3 +490,64 @@ ax2.set_xlim([1, max(num_epochs)])
 plt.xlabel('Epoch')
 plt.ylabel('Validation Loss')
 plt.show()
+
+# Obtain Predictions
+
+def get_predictions(index, partition, input_to_softmax, model_path):
+    """ Print a model's decoded predictions
+    Params:
+        index (int): The example you would like to visualize
+        partition (str): One of 'train' or 'validation'
+        input_to_softmax (Model): The acoustic model
+        model_path (str): Path to saved acoustic model's weights
+    """
+    # load the train and test data
+    data_gen = AudioGenerator()
+    data_gen.load_train_data()
+    data_gen.load_validation_data()
+    
+    # obtain the true transcription and the audio features 
+    if partition == 'validation':
+        transcr = data_gen.valid_texts[index]
+        audio_path = data_gen.valid_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    elif partition == 'train':
+        transcr = data_gen.train_texts[index]
+        audio_path = data_gen.train_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    else:
+        raise Exception('Invalid partition!  Must be "train" or "validation"')
+        
+    # obtain and decode the acoustic model's predictions
+    input_to_softmax.load_weights(model_path)
+    prediction = input_to_softmax.predict(np.expand_dims(data_point, axis=0))
+    output_length = [input_to_softmax.output_length(data_point.shape[0])] 
+    pred_ints = (K.eval(K.ctc_decode(
+                prediction, output_length)[0][0])+1).flatten().tolist()
+    
+    # play the audio file, and display the true and predicted transcriptions
+    print('-'*80)
+    Audio(audio_path)
+    print('True transcription:\n' + '\n' + transcr)
+    print('-'*80)
+    print('Predicted transcription:\n' + '\n' + ''.join(int_sequence_to_text(pred_ints)))
+    print('-'*80)
+    
+get_predictions(index=0, 
+                partition='train',
+                #input_to_softmax=final_model(input_dim=161, # change to 13 if you would like to use MFCC features
+                #                             filters=250, kernel_size=11, 
+                #                             conv_stride=2, conv_border_mode='valid',
+                #                             units=250, recur_layers=3), 
+                
+                input_to_softmax=model_end,
+                model_path='./results/model_end.h5')
+
+get_predictions(index=0, 
+                partition='validation',
+                #input_to_softmax=final_model(input_dim=161, # change to 13 if you would like to use MFCC features
+                #                             filters=200, kernel_size=11, 
+                #                             conv_stride=2, conv_border_mode='valid',
+                #                             units=250, recur_layers=3), 
+                input_to_softmax=model_end,
+                model_path='./results/model_end.h5')
