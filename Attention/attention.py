585f40770d88802d1ea9138340b66d0f4e9177b3
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-17 21:57:12 +0530

    Model Architecture Added

--- Attention/attention.py
+++ Attention/attention.py
@@ -81,3 +81,36 @@ preproc_code_sentences, preproc_plaintext_sentences, code_tokenizer, plaintext_t
     preprocess(codes, plaintext)
 
 print('Data Preprocessed')
+
+from keras.layers import GRU, Input, Dense, TimeDistributed
+from keras.models import Model
+from keras.layers import Activation
+from keras.optimizers import Adam
+from keras.losses import sparse_categorical_crossentropy
+
+
+def simple_model(input_shape, output_sequence_length, code_vocab_size, plaintext_vocab_size):
+    """
+    :param input_shape: Tuple of input shape
+    :param output_sequence_length: Length of output sequence
+    :param code_vocab_size: Number of unique code characters in the dataset
+    :param plaintext_vocab_size: Number of unique plaintext characters in the dataset
+    :return: Keras model built, but not trained
+    """
+    # TODO: Build the layers
+    learning_rate = 1e-3
+
+    input_seq = Input(input_shape[1:])
+    rnn = GRU(64, return_sequences=True)(input_seq)
+    logits = TimeDistributed(Dense(plaintext_vocab_size))(rnn)
+
+    model = Model(input_seq, Activation('softmax')(logits))
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(learning_rate),
+                  metrics=['accuracy'])
+    return model
+
+
+# Reshaping the input to work with a basic RNN
+tmp_x = pad(preproc_code_sentences, preproc_plaintext_sentences.shape[1])
+tmp_x = tmp_x.reshape((-1, preproc_plaintext_sentences.shape[-2], 1))
