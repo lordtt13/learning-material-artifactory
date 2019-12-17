ce8142f6880de34da645f9a5572bf88e3c99deda
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-17 21:54:55 +0530

    Padding function added

--- Attention/attention.py
+++ Attention/attention.py
@@ -37,3 +37,25 @@ for sample_i, (sent, token_sent) in enumerate(zip(text_sentences, text_tokenized
     print('Sequence {} in x'.format(sample_i + 1))
     print('  Input:  {}'.format(sent))
     print('  Output: {}'.format(token_sent))
+    
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+
+
+def pad(x, length=None):
+    """
+    :param x: List of sequences.
+    :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
+    :return: Padded numpy array of sequences
+    """
+    
+    if length is None:
+        length = max([len(sentence) for sentence in x])
+    return pad_sequences(x, maxlen=length, padding='post')
+
+# Pad Tokenized output
+test_pad = pad(text_tokenized)
+for sample_i, (token_sent, pad_sent) in enumerate(zip(text_tokenized, test_pad)):
+    print('Sequence {} in x'.format(sample_i + 1))
+    print('  Input:  {}'.format(np.array(token_sent)))
+    print('  Output: {}'.format(pad_sent))
