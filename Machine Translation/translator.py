6ca27532e86c1b75a3fb8a101c519df8af257b98
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-28 22:51:03 +0530

    Translation project solution added

---
+++ Machine Translation/translator.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 22:39:12 2019
+
+@author: Tanmay Thakur
+"""
+
+import collections
+
+import helper
+import numpy as np
+import project_tests as tests
+
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.losses import sparse_categorical_crossentropy
+
+from keras.callbacks import TensorBoard
+from time import time
+
+
+# Load English data
+english_sentences = helper.load_data('data/small_vocab_en')
+# Load French data
+french_sentences = helper.load_data('data/small_vocab_fr')
+
+english_words_counter = collections.Counter([word for sentence in english_sentences for word in sentence.split()])
+french_words_counter = collections.Counter([word for sentence in french_sentences for word in sentence.split()])
+
+def tokenize(x):
+    """
+    Tokenize x
+    :param x: List of sentences/strings to be tokenized
+    :return: Tuple of (tokenized x data, tokenizer used to tokenize x)
+    """   
+    x_tk = Tokenizer()
+    x_tk.fit_on_texts(x)
+    return x_tk.texts_to_sequences(x), x_tk
+
+tests.test_tokenize(tokenize)
+
+# Tokenize Example output
+text_sentences = [
+    'The quick brown fox jumps over the lazy dog .',
+    'By Jove , my quick study of lexicography won a prize .',
+    'This is a short sentence .']
+text_tokenized, text_tokenizer = tokenize(text_sentences)
+
+def pad(x, length=None):
+    """
+    Pad x
+    :param x: List of sequences.
+    :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
+    :return: Padded numpy array of sequences
+    """
+    if length is None:
+        # Find the length of the longest sequence/sentence
+        length = max([len(seq) for seq in x])
+    
+    return pad_sequences(sequences=x, maxlen=length, padding='post')
+
+tests.test_pad(pad)
+
+# Pad Tokenized output
+test_pad = pad(text_tokenized)
+
+def preprocess(x, y):
+    """
+    Preprocess x and y
+    :param x: Feature List of sentences
+    :param y: Label List of sentences
+    :return: Tuple of (Preprocessed x, Preprocessed y, x tokenizer, y tokenizer)
+    """
+    preprocess_x, x_tk = tokenize(x)
+    preprocess_y, y_tk = tokenize(y)
+
+    preprocess_x = pad(preprocess_x)
+    preprocess_y = pad(preprocess_y)
+
+    # Keras's sparse_categorical_crossentropy function requires the labels to be in 3 dimensions
+    preprocess_y = preprocess_y.reshape(*preprocess_y.shape, 1)
+
+    return preprocess_x, preprocess_y, x_tk, y_tk
+
+preproc_english_sentences, preproc_french_sentences, english_tokenizer, french_tokenizer =\
+    preprocess(english_sentences, french_sentences)
+    
+max_english_sequence_length = preproc_english_sentences.shape[1]
+max_french_sequence_length = preproc_french_sentences.shape[1]
+english_vocab_size = len(english_tokenizer.word_index)
+french_vocab_size = len(french_tokenizer.word_index)
+
+def logits_to_text(logits, tokenizer):
+    """
+    Turn logits from a neural network into text using the tokenizer
+    :param logits: Logits from a neural network
+    :param tokenizer: Keras Tokenizer fit on the labels
+    :return: String that represents the text of the logits
+    """
+    index_to_words = {id: word for word, id in tokenizer.word_index.items()}
+    index_to_words[0] = '<PAD>'
+
+    return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=1, write_graph=True)
+
