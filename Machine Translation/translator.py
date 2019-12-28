26b0177c3af65d9ccdb1450fb0ffed4944a45073
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-29 01:33:32 +0530

    Simple Model Add

--- Machine Translation/translator.py
+++ Machine Translation/translator.py
@@ -109,3 +109,42 @@ def logits_to_text(logits, tokenizer):
 
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=1, write_graph=True)
 
+def simple_model(input_shape, output_sequence_length, english_vocab_size, french_vocab_size):
+    """
+    Build and train a basic RNN on x and y
+    :param input_shape: Tuple of input shape
+    :param output_sequence_length: Length of output sequence
+    :param english_vocab_size: Number of unique English words in the dataset
+    :param french_vocab_size: Number of unique French words in the dataset
+    :return: Keras model built, but not trained
+    """
+    # TODO: Build the layers
+    
+    input_seq = Input(shape=input_shape[1:])
+    rnn = GRU(units=english_vocab_size, return_sequences=True)(input_seq)
+    logits = TimeDistributed(Dense(units=french_vocab_size))(rnn) 
+                             
+    model = Model(input_seq, Activation('softmax')(logits))
+
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(lr=1e-3),
+                  metrics=['accuracy'])
+    return model
+
+tests.test_simple_model(simple_model)
+
+# Pad and Reshape the input to work with a basic RNN
+tmp_x = pad(preproc_english_sentences, max_french_sequence_length)
+tmp_x = tmp_x.reshape((-1, preproc_french_sentences.shape[-2], 1))
+
+# Train the neural network
+simple_rnn_model = simple_model(
+    tmp_x.shape,
+    max_french_sequence_length,
+    english_vocab_size+1,
+    french_vocab_size+1)
+
+simple_rnn_model.fit(tmp_x, preproc_french_sentences, batch_size=1024, epochs=10, validation_split=0.2)
+
+# Print prediction(s)
+print(logits_to_text(simple_rnn_model.predict(tmp_x[:1])[0], french_tokenizer))
