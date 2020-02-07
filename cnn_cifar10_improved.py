092d22bc27f3960c202587e0896720b2a94c95c2
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-07 12:48:02 +0530

    Confusion matrix, model summary add

--- cnn_cifar10_improved.py
+++ cnn_cifar10_improved.py
@@ -80,3 +80,61 @@ plt.plot(r.history['accuracy'], label='acc')
 plt.plot(r.history['val_accuracy'], label='val_acc')
 plt.legend()
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+  """
+  This function prints and plots the confusion matrix.
+  Normalization can be applied by setting `normalize=True`.
+  """
+  if normalize:
+      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+      print("Normalized confusion matrix")
+  else:
+      print('Confusion matrix, without normalization')
+
+  print(cm)
+
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=45)
+  plt.yticks(tick_marks, classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, format(cm[i, j], fmt),
+               horizontalalignment="center",
+               color="white" if cm[i, j] > thresh else "black")
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  plt.show()
+
+
+p_test = model.predict(x_test).argmax(axis=1)
+cm = confusion_matrix(y_test, p_test)
+plot_confusion_matrix(cm, list(range(10)))
+
+# label mapping
+labels = '''airplane
+automobile
+bird
+cat
+deer
+dog
+frog
+horse
+ship
+truck'''.split()
+
+misclassified_idx = np.where(p_test != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(x_test[i], cmap='gray')
+plt.title("True label: %s Predicted: %s" % (labels[y_test[i]], labels[p_test[i]]));
+
+model.summary()
