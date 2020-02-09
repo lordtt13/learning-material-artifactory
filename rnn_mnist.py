076469357ba27bd3298b96510fa5a4fc0fd52e1b
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-10 00:53:24 +0530

    Confusion Matrix, Visualization Add

--- rnn_mnist.py
+++ rnn_mnist.py
@@ -4,10 +4,11 @@ Created on Mon Feb 10 00:45:10 2020
 
 @author: Tanmay Thakur
 """
-
+import itertools
 import tensorflow as tf
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
+from sklearn.metrics import confusion_matrix
 from tensorflow.keras.layers import Input, Dense, SimpleRNN, Flatten, GRU, LSTM, GlobalMaxPool1D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD, Adam
@@ -39,3 +40,51 @@ plt.legend()
 plt.plot(r.history['accuracy'], label='acc')
 plt.plot(r.history['val_accuracy'], label='val_acc')
 plt.legend()
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+  """
+  This function prints and plots the confusion matrix.
+  Normalization can be applied by setting `normalize=True`.
+  """
+  if normalize:
+      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+      print("Normalized confusion matrix")
+  else:
+      print('Confusion matrix, without normalization')
+
+  print(cm)
+
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=45)
+  plt.yticks(tick_marks, classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, format(cm[i, j], fmt),
+               horizontalalignment="center",
+               color="white" if cm[i, j] > thresh else "black")
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  plt.show()
+
+
+p_test = model.predict(x_test).argmax(axis=1)
+cm = confusion_matrix(y_test, p_test)
+plot_confusion_matrix(cm, list(range(10)))
+
+# It's easy to confuse 9 <--> 4, 9 <--> 7, 2 <--> 7, etc. 
+
+# Show some misclassified examples
+misclassified_idx = np.where(p_test != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(x_test[i], cmap='gray')
+plt.title("True label: %s Predicted: %s" % (y_test[i], p_test[i]));
