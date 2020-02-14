a1777d962b683f12063679d942de708d3f0dec52
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-14 12:41:34 +0530

    Plot loss and accuracy

--- spam_rnn.py
+++ spam_rnn.py
@@ -94,3 +94,10 @@ r = model.fit(
   validation_data=(data_test, Ytest)
 )
 
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend()
