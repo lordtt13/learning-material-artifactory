3a04379b8b23023aeb930ce99193cfa8ead90a18
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-16 11:46:14 +0530

    Data moved to be compatible with flow_from_directory func

---
+++ transfer_learning_with_data_aug.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 11:40:57 2020
+
+@author: Tanmay Thakur
+"""
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys, os
+
+from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.applications.vgg16 import VGG16 as PretrainedModel, preprocess_input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from glob import glob
+
+
+# Data from: https://mmspg.epfl.ch/downloads/food-image-datasets/
+# !wget --passive-ftp --prefer-family=ipv4 --ftp-user FoodImage@grebvm2.epfl.ch \
+#  --ftp-password Cahc1moo -nc ftp://tremplin.epfl.ch/Food-5K.zip
+!wget -nc https://lazyprogrammer.me/course_files/Food-5K.zip
+
+!unzip -qq -o Food-5K.zip
+
+!ls Food-5K/training
+
+!mv Food-5K/* .
+
+!mkdir data
+
+# Make directories to store the data Keras-style
+!mkdir data/train
+!mkdir data/test
+!mkdir data/train/nonfood
+!mkdir data/train/food
+!mkdir data/test/nonfood
+!mkdir data/test/food
+
+# Move the images
+# Note: we will consider 'training' to be the train set
+#       'validation' folder will be the test set
+#       ignore the 'evaluation' set
+!mv training/0*.jpg data/train/nonfood
+!mv training/1*.jpg data/train/food
+!mv validation/0*.jpg data/test/nonfood
+!mv validation/1*.jpg data/test/food
+
+train_path = 'data/train'
+valid_path = 'data/test'
+
+# These images are pretty big and of different sizes
+# Let's load them all in as the same (smaller) size
+IMAGE_SIZE = [200, 200]
+
+# useful for getting number of files
+image_files = glob(train_path + '/*/*.jpg')
+valid_image_files = glob(valid_path + '/*/*.jpg')
+
+# useful for getting number of classes
+folders = glob(train_path + '/*')
+folders
+
