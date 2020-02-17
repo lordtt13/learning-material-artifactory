68321842116237bb34527cb2a5085299ccbd45fc
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-17 22:59:23 +0530

    Transfer learning initialized without data aug

---
+++ transfer_learning.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 22:54:34 2020
+
+@author: Tanmay Thakur
+"""
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys, os
+
+from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.applications.vgg16 import VGG16 as PretrainedModel, preprocess_input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from glob import glob
+
+
+# Data from: https://mmspg.epfl.ch/downloads/food-image-datasets/
+# !wget --passive-ftp --prefer-family=ipv4 --ftp-user FoodImage@grebvm2.epfl.ch \
+#  --ftp-password Cahc1moo -nc ftp://tremplin.epfl.ch/Food-5K.zip
+!wget -nc https://lazyprogrammer.me/course_files/Food-5K.zip
+
+!unzip -qq -o Food-5K.zip
+
+!ls Food-5K/training
+
+!mv Food-5K/* .
+
+!mkdir data
+
+# Make directories to store the data Keras-style
+!mkdir data/train
+!mkdir data/test
+!mkdir data/train/nonfood
+!mkdir data/train/food
+!mkdir data/test/nonfood
+!mkdir data/test/food
+
+# Move the images
+# Note: we will consider 'training' to be the train set
+#       'validation' folder will be the test set
+#       ignore the 'evaluation' set
+!mv training/0*.jpg data/train/nonfood
+!mv training/1*.jpg data/train/food
+!mv validation/0*.jpg data/test/nonfood
+!mv validation/1*.jpg data/test/food
+
+train_path = 'data/train'
+valid_path = 'data/test'
+
+# These images are pretty big and of different sizes
+# Let's load them all in as the same (smaller) size
+IMAGE_SIZE = [200, 200]
+
+# useful for getting number of files
+image_files = glob(train_path + '/*/*.jpg')
+valid_image_files = glob(valid_path + '/*/*.jpg')
+
+# useful for getting number of classes
+folders = glob(train_path + '/*')
+folders
+
+ptm = PretrainedModel(
+    input_shape=IMAGE_SIZE + [3],
+    weights='imagenet',
+    include_top=False)
+
+# freeze pretrained model weights
+ptm.trainable = False
+
+# map the data into feature vectors
+
+# Keras image data generator returns classes one-hot encoded
+
+K = len(folders) # number of classes
+x = Flatten()(ptm.output)
+x = Dense(K, activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=ptm.input, outputs=x)
+
+model.summary()
+
+# create an instance of ImageDataGenerator
+gen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+batch_size = 128
+
+# create generators
+train_generator = gen.flow_from_directory(
+  train_path,
+  target_size=IMAGE_SIZE,
+  batch_size=batch_size,
+  class_mode='binary',
+)
+
+valid_generator = gen.flow_from_directory(
+  valid_path,
+  target_size=IMAGE_SIZE,
+  batch_size=batch_size,
+  class_mode='binary',
+)
+
+Ntrain = len(image_files)
+Nvalid = len(valid_image_files)
+
+# Figure out the output size
+feat = model.predict(np.random.random([1] + IMAGE_SIZE + [3]))
+D = feat.shape[1]
+
+X_train = np.zeros((Ntrain, D))
+Y_train = np.zeros(Ntrain)
+X_valid = np.zeros((Nvalid, D))
+Y_valid = np.zeros(Nvalid)
+
