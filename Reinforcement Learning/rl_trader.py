31538771c313e30571c9a5e95c60cf432f6da22d
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-02 22:42:57 +0530

    Agent class add
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

--- Reinforcement Learning/rl_trader.py
+++ Reinforcement Learning/rl_trader.py
@@ -233,3 +233,85 @@ class MultiStockEnv:
             self.cash_in_hand -= self.stock_price[i]
           else:
             can_buy = False
+            
+class DQNAgent(object):
+  def __init__(self, state_size, action_size):
+    self.state_size = state_size
+    self.action_size = action_size
+    self.memory = ReplayBuffer(state_size, action_size, size=500)
+    self.gamma = 0.95  # discount rate
+    self.epsilon = 1.0  # exploration rate
+    self.epsilon_min = 0.01
+    self.epsilon_decay = 0.995
+    self.model = mlp(state_size, action_size)
+
+
+  def update_replay_memory(self, state, action, reward, next_state, done):
+    self.memory.store(state, action, reward, next_state, done)
+
+
+  def act(self, state):
+    if np.random.rand() <= self.epsilon:
+      return np.random.choice(self.action_size)
+    act_values = self.model.predict(state)
+    return np.argmax(act_values[0])  # returns action
+
+
+  def replay(self, batch_size=32):
+    # first check if replay buffer contains enough data
+    if self.memory.size < batch_size:
+      return
+
+    # sample a batch of data from the replay memory
+    minibatch = self.memory.sample_batch(batch_size)
+    states = minibatch['s']
+    actions = minibatch['a']
+    rewards = minibatch['r']
+    next_states = minibatch['s2']
+    done = minibatch['d']
+
+    # Calculate the tentative target: Q(s',a)
+    target = rewards + (1 - done) * self.gamma * np.amax(self.model.predict(next_states), axis=1)
+
+    # With the Keras API, the target (usually) must have the same
+    # shape as the predictions.
+    # However, we only need to update the network for the actions
+    # which were actually taken.
+    # We can accomplish this by setting the target to be equal to
+    # the prediction for all values.
+    # Then, only change the targets for the actions taken.
+    # Q(s,a)
+    target_full = self.model.predict(states)
+    target_full[np.arange(batch_size), actions] = target
+
+    # Run one training step
+    self.model.train_on_batch(states, target_full)
+
+    if self.epsilon > self.epsilon_min:
+      self.epsilon *= self.epsilon_decay
+
+
+  def load(self, name):
+    self.model.load_weights(name)
+
+
+  def save(self, name):
+    self.model.save_weights(name)
+
+
+def play_one_episode(agent, env, is_train):
+  # note: after transforming states are already 1xD
+  state = env.reset()
+  state = scaler.transform([state])
+  done = False
+
+  while not done:
+    action = agent.act(state)
+    next_state, reward, done, info = env.step(action)
+    next_state = scaler.transform([next_state])
+    if is_train == 'train':
+      agent.update_replay_memory(state, action, reward, next_state, done)
+      agent.replay(batch_size)
+    state = next_state
+
+  return info['cur_val']
