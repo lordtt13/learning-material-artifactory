b366c23f2d16add1fec8dcf7ffdf3fd8fdef58bb
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-02 22:34:33 +0530

    Replay Buffer Class Add
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

--- Reinforcement Learning/rl_trader.py
+++ Reinforcement Learning/rl_trader.py
@@ -23,3 +23,30 @@ def get_data():
   df = pd.read_csv('aapl_msi_sbux.csv')
   return df.values
 
+
+class ReplayBuffer:
+  def __init__(self, obs_dim, act_dim, size):
+    self.obs1_buf = np.zeros([size, obs_dim], dtype=np.float32)
+    self.obs2_buf = np.zeros([size, obs_dim], dtype=np.float32)
+    self.acts_buf = np.zeros(size, dtype=np.uint8)
+    self.rews_buf = np.zeros(size, dtype=np.float32)
+    self.done_buf = np.zeros(size, dtype=np.uint8)
+    self.ptr, self.size, self.max_size = 0, 0, size
+
+  def store(self, obs, act, rew, next_obs, done):
+    self.obs1_buf[self.ptr] = obs
+    self.obs2_buf[self.ptr] = next_obs
+    self.acts_buf[self.ptr] = act
+    self.rews_buf[self.ptr] = rew
+    self.done_buf[self.ptr] = done
+    self.ptr = (self.ptr+1) % self.max_size
+    self.size = min(self.size+1, self.max_size)
+
+  def sample_batch(self, batch_size=32):
+    idxs = np.random.randint(0, self.size, size=batch_size)
+    return dict(s=self.obs1_buf[idxs],
+                s2=self.obs2_buf[idxs],
+                a=self.acts_buf[idxs],
+                r=self.rews_buf[idxs],
+                d=self.done_buf[idxs])
+
