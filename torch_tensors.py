ced2e1f0430b94b11a9ef33fbf299ea00951880a
lordtt13 <thakurtanmay72@yahoo.com>
2020-03-26 15:46:38 +0530

    Torch Tensor types add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- torch_tensors.py
+++ torch_tensors.py
@@ -41,3 +41,27 @@ print(x2.type())
 arr = np.arange(0,5)
 t = torch.tensor(arr)
 print(t)
+
+
+"""
+Class constructors
+torch.Tensor()
+torch.FloatTensor()
+torch.LongTensor(), etc.
+
+There's a subtle difference between using the factory function torch.tensor(data) and the class constructor torch.Tensor(data).
+The factory function determines the dtype from the incoming data, or from a passed-in dtype argument.
+The class constructor torch.Tensor()is simply an alias for torch.FloatTensor(data). 
+"""
+
+data = np.array([1,2,3])
+
+a = torch.Tensor(data)  # Equivalent to cc = torch.FloatTensor(data)
+print(a, a.type())
+
+b = torch.tensor(data)
+print(b, b.type())
+
+c = torch.tensor(data, dtype=torch.long)
+print(c, c.type())
+
