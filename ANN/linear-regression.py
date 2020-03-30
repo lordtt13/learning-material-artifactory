be1d82af4f99869673cf760e9c13e3103eaacca1
lordtt13 <thakurtanmay72@yahoo.com>
2020-03-30 06:15:11 +0530

    Training Added with results on LR Linear Model
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- ANN/linear-regression.py
+++ ANN/linear-regression.py
@@ -11,6 +11,8 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
+
 
 torch.manual_seed(42)
 
@@ -69,6 +71,49 @@ print(y1)
 plt.scatter(X.numpy(), y.numpy())
 plt.plot(x1,y1,'r')
 plt.title('Initial Model')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.plot()
+
+# Set Loss
+criterion = nn.MSELoss()
+
+# Set Optimizer
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.001)
+
+epochs = 50
+losses = []
+
+for i in tqdm(range(epochs)):
+    i+=1
+    y_pred = model.forward(X)
+    loss = criterion(y_pred, y)
+    losses.append(loss)
+    print(f'epoch: {i:2}  loss: {loss.item():10.4f}  weight: {model.linear.weight.item():10.4f}  \
+bias: {model.linear.bias.item():10.4f}') 
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+  
+# Plot Losses
+plt.plot(range(epochs), losses)
+plt.ylabel('Loss')
+plt.xlabel('epoch')
+plt.plot()
+
+# Plot Results
+w1,b1 = model.linear.weight.item(), model.linear.bias.item()
+print(f'Current weight: {w1:.8f}, Current bias: {b1:.8f}')
+print()
+
+y1 = x1*w1 + b1
+print(x1)
+print(y1)
+
+plt.scatter(X.numpy(), y.numpy())
+plt.plot(x1,y1,'r')
+plt.title('Current Model')
 plt.ylabel('y')
 plt.xlabel('x')
 plt.plot()
