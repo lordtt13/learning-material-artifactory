fd785c7b04bd159ee635e5b3ded8581976013998
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-12 11:32:59 +0530

    Save, load and custom_predict func add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- ANN/ann-regression.py
+++ ANN/ann-regression.py
@@ -245,3 +245,58 @@ for i in range(50):
     diff = np.abs(y_val[i].item()-y_test[i].item())
     print(f'{i+1:2}. {y_val[i].item():8.4f} {y_test[i].item():8.4f} {diff:8.4f}')
     
+    
+# Save model
+# Make sure to save the model only after the training has happened!
+if len(losses) == epochs:
+    torch.save(model.state_dict(), 'TaxiFareRegrModel.pt')
+else:
+    print('Model has not been trained. Consider loading a trained model instead.')
+    
+# Loading the model
+model2 = TabularModel(emb_szs, 6, 1, [200,100], p=0.4)
+
+model2.load_state_dict(torch.load('TaxiFareRegrModel.pt'));
+model2.eval()
+
+# Custom prediction function
+def test_data(mdl): # pass in the name of the new model
+    # INPUT NEW DATA
+    plat = float(input('What is the pickup latitude?  '))
+    plong = float(input('What is the pickup longitude? '))
+    dlat = float(input('What is the dropoff latitude?  '))
+    dlong = float(input('What is the dropoff longitude? '))
+    psngr = int(input('How many passengers? '))
+    dt = input('What is the pickup date and time?\nFormat as YYYY-MM-DD HH:MM:SS     ')
+    
+    # PREPROCESS THE DATA
+    dfx_dict = {'pickup_latitude':plat,'pickup_longitude':plong,'dropoff_latitude':dlat,
+         'dropoff_longitude':dlong,'passenger_count':psngr,'EDTdate':dt}
+    dfx = pd.DataFrame(dfx_dict, index=[0])
+    dfx['dist_km'] = haversine_distance(dfx,'pickup_latitude', 'pickup_longitude',
+                                        'dropoff_latitude', 'dropoff_longitude')
+    dfx['EDTdate'] = pd.to_datetime(dfx['EDTdate'])
+    
+    # We can skip the .astype(category) step since our fields are small,
+    # and encode them right away
+    dfx['Hour'] = dfx['EDTdate'].dt.hour
+    dfx['AMorPM'] = np.where(dfx['Hour']<12,0,1) 
+    dfx['Weekday'] = dfx['EDTdate'].dt.strftime("%a")
+    dfx['Weekday'] = dfx['Weekday'].replace(['Fri','Mon','Sat','Sun','Thu','Tue','Wed'],
+                                            [0,1,2,3,4,5,6]).astype('int64')
+    # CREATE CAT AND CONT TENSORS
+    cat_cols = ['Hour', 'AMorPM', 'Weekday']
+    cont_cols = ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude',
+                 'dropoff_longitude', 'passenger_count', 'dist_km']
+    xcats = np.stack([dfx[col].values for col in cat_cols], 1)
+    xcats = torch.tensor(xcats, dtype=torch.int64)
+    xconts = np.stack([dfx[col].values for col in cont_cols], 1)
+    xconts = torch.tensor(xconts, dtype=torch.float)
+    
+    # PASS NEW DATA THROUGH THE MODEL WITHOUT PERFORMING A BACKPROP
+    with torch.no_grad():
+        z = mdl(xcats, xconts)
+    print(f'\nThe predicted fare amount is ${z.item():.2f}')
+    
+# Test custom prediction
+z = test_data(model2)
