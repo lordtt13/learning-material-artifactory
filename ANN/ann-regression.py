4efe08f331d73aa2faf0a1fbcc5aba3051c422b3
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-12 11:23:24 +0530

    Model Class Instantiated
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- ANN/ann-regression.py
+++ ANN/ann-regression.py
@@ -140,3 +140,39 @@ selfembdrop = nn.Dropout(.4)
 
 z = selfembdrop(z)
 z
+
+
+# Instantiate Model
+class TabularModel(nn.Module):
+
+    def __init__(self, emb_szs, n_cont, out_sz, layers, p=0.5):
+        super().__init__()
+        self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
+        self.emb_drop = nn.Dropout(p)
+        self.bn_cont = nn.BatchNorm1d(n_cont)
+        
+        layerlist = []
+        n_emb = sum((nf for ni,nf in emb_szs))
+        n_in = n_emb + n_cont
+        
+        for i in layers:
+            layerlist.append(nn.Linear(n_in,i)) 
+            layerlist.append(nn.ReLU(inplace=True))
+            layerlist.append(nn.BatchNorm1d(i))
+            layerlist.append(nn.Dropout(p))
+            n_in = i
+        layerlist.append(nn.Linear(layers[-1],out_sz))
+            
+        self.layers = nn.Sequential(*layerlist)
+    
+    def forward(self, x_cat, x_cont):
+        embeddings = []
+        for i,e in enumerate(self.embeds):
+            embeddings.append(e(x_cat[:,i]))
+        x = torch.cat(embeddings, 1)
+        x = self.emb_drop(x)
+        
+        x_cont = self.bn_cont(x_cont)
+        x = torch.cat([x, x_cont], 1)
+        x = self.layers(x)
+        return x
