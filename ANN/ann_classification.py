8da65cfa0abb2599ca46a5356894fb18b491b8cc
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-17 10:37:17 +0530

    Model definition, train, split, validate, plot metrics, custom predict add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- ANN/ann_classification.py
+++ ANN/ann_classification.py
@@ -6,6 +6,7 @@ Created on Sat Apr 18 10:26:04 2020
 @author: tanmay
 """
 
+import time
 import torch
 
 import numpy as np
@@ -87,3 +88,143 @@ cat_szs = [len(df[col].cat.categories) for col in cat_cols]
 emb_szs = [(size, min(50, (size+1)//2)) for size in cat_szs]
 emb_szs
 
+# Define TabularModel
+class TabularModel(nn.Module):
+
+    def __init__(self, emb_szs, n_cont, out_sz, layers, p=0.5):
+        super().__init__()
+        self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
+        self.emb_drop = nn.Dropout(p)
+        self.bn_cont = nn.BatchNorm1d(n_cont)
+        
+        layerlist = []
+        n_emb = sum((nf for ni,nf in emb_szs))
+        n_in = n_emb + n_cont
+        
+        for i in layers:
+            layerlist.append(nn.Linear(n_in,i)) 
+            layerlist.append(nn.ReLU(inplace=True))
+            layerlist.append(nn.BatchNorm1d(i))
+            layerlist.append(nn.Dropout(p))
+            n_in = i
+        layerlist.append(nn.Linear(layers[-1],out_sz))
+            
+        self.layers = nn.Sequential(*layerlist)
+    
+    def forward(self, x_cat, x_cont):
+        embeddings = []
+        for i,e in enumerate(self.embeds):
+            embeddings.append(e(x_cat[:,i]))
+        x = torch.cat(embeddings, 1)
+        x = self.emb_drop(x)
+        
+        x_cont = self.bn_cont(x_cont)
+        x = torch.cat([x, x_cont], 1)
+        x = self.layers(x)
+        return x
+    
+model = TabularModel(emb_szs, conts.shape[1], out_sz = 2, [200, 100], p = 0.4)
+
+model
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+batch_size = 60000
+test_size = 12000
+
+cat_train = cats[:batch_size-test_size]
+cat_test = cats[batch_size-test_size:batch_size]
+con_train = conts[:batch_size-test_size]
+con_test = conts[batch_size-test_size:batch_size]
+y_train = y[:batch_size-test_size]
+y_test = y[batch_size-test_size:batch_size]
+
+len(cat_train), len(cat_test)
+
+start_time = time.time()
+
+epochs = 300
+losses = []
+
+for i in range(epochs):
+    i+=1
+    y_pred = model(cat_train, con_train)
+    loss = criterion(y_pred, y_train)
+    losses.append(loss)
+    
+    # a neat trick to save screen space:
+    if i%25 == 1:
+        print(f'epoch: {i:3}  loss: {loss.item():10.8f}')
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print(f'epoch: {i:3}  loss: {loss.item():10.8f}') 
+print(f'\nDuration: {time.time() - start_time:.0f} seconds') 
+
+# Plot metrics
+plt.plot(range(epochs), losses)
+plt.ylabel('Cross Entropy Loss')
+plt.xlabel('epoch')
+
+# Validate
+with torch.no_grad():
+    y_val = model(cat_test, con_test)
+    loss = criterion(y_val, y_test)
+print(f'CE Loss: {loss:.8f}')
+
+rows = 50
+correct = 0
+print(f'{"MODEL OUTPUT":26} ARGMAX  Y_TEST')
+for i in range(rows):
+    print(f'{str(y_val[i]):26} {y_val[i].argmax():^7}{y_test[i]:^7}')
+    if y_val[i].argmax().item() == y_test[i]:
+        correct += 1
+print(f'\n{correct} out of {rows} = {100*correct/rows:.2f}% correct')
+
+# Save Model
+if len(losses) == epochs:
+    torch.save(model.state_dict(), 'TaxiFareClssModel.pt')
+else:
+    print('Model has not been trained. Consider loading a trained model instead.')
+    
+# Custom predict
+def test_data(mdl): # pass in the name of the new model
+    # INPUT NEW DATA
+    plat = float(input('What is the pickup latitude?  '))
+    plong = float(input('What is the pickup longitude? '))
+    dlat = float(input('What is the dropoff latitude?  '))
+    dlong = float(input('What is the dropoff longitude? '))
+    psngr = int(input('How many passengers? '))
+    dt = input('What is the pickup date and time?\nFormat as YYYY-MM-DD HH:MM:SS     ')
+    
+    # PREPROCESS THE DATA
+    dfx_dict = {'pickup_latitude':plat,'pickup_longitude':plong,'dropoff_latitude':dlat,
+         'dropoff_longitude':dlong,'passenger_count':psngr,'EDTdate':dt}
+    dfx = pd.DataFrame(dfx_dict, index=[0])
+    dfx['dist_km'] = haversine_distance(dfx,'pickup_latitude', 'pickup_longitude',
+                                        'dropoff_latitude', 'dropoff_longitude')
+    dfx['EDTdate'] = pd.to_datetime(dfx['EDTdate'])
+    
+    # We can skip the .astype(category) step since our fields are small,
+    # and encode them right away
+    dfx['Hour'] = dfx['EDTdate'].dt.hour
+    dfx['AMorPM'] = np.where(dfx['Hour']<12,0,1) 
+    dfx['Weekday'] = dfx['EDTdate'].dt.strftime("%a")
+    dfx['Weekday'] = dfx['Weekday'].replace(['Fri','Mon','Sat','Sun','Thu','Tue','Wed'],
+                                            [0,1,2,3,4,5,6]).astype('int64')
+    # CREATE CAT AND CONT TENSORS
+    cat_cols = ['Hour', 'AMorPM', 'Weekday']
+    cont_cols = ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude',
+                 'dropoff_longitude', 'passenger_count', 'dist_km']
+    xcats = np.stack([dfx[col].values for col in cat_cols], 1)
+    xcats = torch.tensor(xcats, dtype=torch.int64)
+    xconts = np.stack([dfx[col].values for col in cont_cols], 1)
+    xconts = torch.tensor(xconts, dtype=torch.float)
+    
+    # PASS NEW DATA THROUGH THE MODEL WITHOUT PERFORMING A BACKPROP
+    with torch.no_grad():
+        z = mdl(xcats, xconts).argmax().item()
+    print(f'\nThe predicted fare class is {z}')
