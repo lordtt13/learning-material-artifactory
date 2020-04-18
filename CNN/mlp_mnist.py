22aa6b6518773ae2ffb6fc7ee255430c4210b889
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-18 11:04:38 +0530

    Train, plot, evaluate add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- CNN/mlp_mnist.py
+++ CNN/mlp_mnist.py
@@ -89,3 +89,120 @@ optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 for images, labels in train_loader:
     print('Batch shape:', images.size())
     break
+
+# EQUIVALENT TO:
+# dataiter = iter(train_loader)
+# images, labels = dataiter.next()
+# print('Batch shape:', images.size())
+    
+images.view(100,-1).size()
+
+start_time = time.time()
+
+epochs = 10
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+    
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        b += 1
+        
+        # Apply the model
+        y_pred = model(X_train.view(100, -1))  
+        loss = criterion(y_pred, y_train)
+ 
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+        
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Print interim results
+        if b%200 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{100*b:6}/60000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item()*100/(100*b):7.3f}%')
+    
+    # Update train loss & accuracy for the epoch
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+        
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+
+            # Apply the model
+            y_val = model(X_test.view(500, -1))  
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1] 
+            tst_corr += (predicted == y_test).sum()
+    
+    # Update test loss & accuracy for the epoch
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+        
+print(f'\nDuration: {time.time() - start_time:.0f} seconds') 
+
+# Plot metrics
+plt.plot(train_losses, label = 'training loss')
+plt.plot(test_losses, label = 'validation loss')
+plt.title('Loss at the end of each epoch')
+plt.legend()
+
+plt.plot([t/600 for t in train_correct], label = 'training accuracy')
+plt.plot([t/100 for t in test_correct], label = 'validation accuracy')
+plt.title('Accuracy at the end of each epoch')
+plt.legend()
+
+# Evaluate
+print(test_correct) 
+print()
+print(f'Test accuracy: {test_correct[-1].item()*100/10000:.3f}%') # print the most recent result as a percent
+
+# Extract the data all at once, not in batches
+test_load_all = DataLoader(test_data, batch_size = 10000, shuffle = False)
+
+with torch.no_grad():
+    correct = 0
+    for X_test, y_test in test_load_all:
+        y_val = model(X_test.view(len(X_test), -1))  
+        predicted = torch.max(y_val,1)[1]
+        correct += (predicted == y_test).sum()
+print(f'Test accuracy: {correct.item()}/{len(test_data)} = {correct.item()*100/(len(test_data)):7.3f}%')
+
+# print the confusion matrix
+print(confusion_matrix(predicted.view(-1), y_test.view(-1)))
+
+# Examine the misses
+misses = np.array([])
+for i in range(len(predicted.view(-1))):
+    if predicted[i] != y_test[i]:
+        misses = np.append(misses,i).astype('int64')
+        
+# Display the number of misses
+len(misses)
+
+# Set up an iterator to feed batched rows
+r = 12   # row size
+row = iter(np.array_split(misses,len(misses)//r+1))
+
+nextrow = next(row)
+print("Index:", nextrow)
+print("Label:", y_test.index_select(0,torch.tensor(nextrow)).numpy())
+print("Guess:", predicted.index_select(0,torch.tensor(nextrow)).numpy())
+
+images = X_test.index_select(0,torch.tensor(nextrow))
+im = make_grid(images, nrow = r)
+plt.figure(figsize = (10,4))
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
