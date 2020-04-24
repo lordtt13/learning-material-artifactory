97eafc8a015165edd07c5925c5c57db9bb0609ae
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-25 02:10:38 +0530

    Train and plots add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- CNN/cnn_mnist.py
+++ CNN/cnn_mnist.py
@@ -6,7 +6,7 @@ Created on Sat Apr 25 01:59:56 2020
 @author: tanmay
 """
 
-
+import time
 import torch
 
 import numpy as np
@@ -64,3 +64,78 @@ def count_parameters(model):
     
 count_parameters(model)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+# Train
+
+start_time = time.time()
+
+epochs = 5
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+    
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        b += 1
+        
+        # Apply the model
+        y_pred = model(X_train)  # we don't flatten X-train here
+        loss = criterion(y_pred, y_train)
+ 
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+        
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Print interim results
+        if b%600 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10*b:6}/60000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item()*100/(10*b):7.3f}%')
+        
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+        
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+
+            # Apply the model
+            y_val = model(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1] 
+            tst_corr += (predicted == y_test).sum()
+            
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+        
+print(f'\nDuration: {time.time() - start_time:.0f} seconds') 
+
+# Plot metrics
+
+plt.plot(train_losses, label = 'training loss')
+plt.plot(test_losses, label = 'validation loss')
+plt.title('Loss at the end of each epoch')
+plt.legend()
+plt.show()
+
+test_losses
+
+plt.plot([t/600 for t in train_correct], label = 'training accuracy')
+plt.plot([t/100 for t in test_correct], label = 'validation accuracy')
+plt.title('Accuracy at the end of each epoch')
+plt.legend()
+plt.show()
