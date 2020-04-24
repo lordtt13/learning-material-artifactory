721f0d8c7365363deb2fbdeefbdc82c54b97ced3
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-25 02:05:31 +0530

    Create a CNN model
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- CNN/cnn_mnist.py
+++ CNN/cnn_mnist.py
@@ -30,3 +30,37 @@ test_data = datasets.MNIST(root = '../Data', train = False, download = True, tra
 
 train_loader = DataLoader(train_data, batch_size = 10, shuffle = True)
 test_loader = DataLoader(test_data, batch_size = 10, shuffle = False)
+
+# Create CNN
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84,10)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 5*5*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim = 1)
+    
+model = ConvolutionalNetwork()
+model
+
+def count_parameters(model):
+    params = [p.numel() for p in model.parameters() if p.requires_grad]
+    for item in params:
+        print(f'{item:>6}')
+    print(f'______\n{sum(params):>6}')
+    
+count_parameters(model)
+
