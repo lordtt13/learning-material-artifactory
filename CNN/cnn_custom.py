715a2efc5a0ce6ea0783f9e435cf653ebc29887a
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-30 01:32:46 +0530

    Train, Save and plot metrics
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- CNN/cnn_custom.py
+++ CNN/cnn_custom.py
@@ -117,3 +117,92 @@ def count_parameters(model):
     print(f'________\n{sum(params):>8}')
     
 count_parameters(CNNmodel)
+
+# Train
+
+start_time = time.time()
+
+epochs = 3
+
+max_trn_batch = 800
+max_tst_batch = 300
+
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+    
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        
+        # Limit the number of batches
+        if b == max_trn_batch:
+            break
+        b+=1
+        
+        # Apply the model
+        y_pred = CNNmodel(X_train)
+        loss = criterion(y_pred, y_train)
+ 
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+        
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Print interim results
+        if b%200 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10*b:6}/8000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item()*100/(10*b):7.3f}%')
+
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+            # Limit the number of batches
+            if b == max_tst_batch:
+                break
+
+            # Apply the model
+            y_val = CNNmodel(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1] 
+            tst_corr += (predicted == y_test).sum()
+
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+
+print(f'\nDuration: {time.time() - start_time:.0f} seconds') 
+
+# Save
+
+torch.save(CNNmodel.state_dict(), 'CustomImageCNNModel.pt')
+
+# Plot metrics
+
+plt.plot(train_losses, label = 'training loss')
+plt.plot(test_losses, label = 'validation loss')
+plt.title('Loss at the end of each epoch')
+plt.legend()
+plt.show()
+
+plt.plot([t/80 for t in train_correct], label = 'training accuracy')
+plt.plot([t/30 for t in test_correct], label = 'validation accuracy')
+plt.title('Accuracy at the end of each epoch')
+plt.legend()
+plt.show()
+
+print(test_correct)
+print(f'Test accuracy: {test_correct[-1].item()*100/3000:.3f}%')
