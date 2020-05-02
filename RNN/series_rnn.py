9f263800a3dca9efe68839a11b0b5f013dfe2c94
lordtt13 <thakurtanmay72@yahoo.com>
2020-05-02 06:46:32 +0530

    Forecast into unknown future
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- RNN/series_rnn.py
+++ RNN/series_rnn.py
@@ -154,7 +154,7 @@ true_predictions, df['S4248SM144NCEN'][-12:]
 
 # Plot the results
 
-x = np.arange('2018-02-01', '2019-02-01', dtype='datetime64[M]').astype('datetime64[D]')
+x = np.arange('2018-02-01', '2019-02-01', dtype = 'datetime64[M]').astype('datetime64[D]')
 
 plt.figure(figsize = (12,4))
 plt.title('Beer, Wine, and Alcohol Sales')
@@ -176,4 +176,74 @@ fig.autofmt_xdate()
 # Select the end of the graph with slice notation:
 plt.plot(df['S4248SM144NCEN']['2017-01-01':])
 plt.plot(x,true_predictions)
+plt.show()
+
+# Plot into unknown future
+
+epochs = 100
+
+# set model to back to training mode
+model.train()
+
+# feature scale the entire dataset
+y_norm = scaler.fit_transform(y.reshape(-1, 1))
+y_norm = torch.FloatTensor(y_norm).view(-1)
+all_data = input_data(y_norm,window_size)
+
+import time
+start_time = time.time()
+
+for epoch in range(epochs):
+    
+    # train on the full set of sequences
+    for seq, y_train in all_data:  
+        
+        # reset the parameters and hidden states
+        optimizer.zero_grad()
+        model.hidden = (torch.zeros(1,1,model.hidden_size),
+                        torch.zeros(1,1,model.hidden_size))
+        
+        y_pred = model(seq)
+        
+        loss = criterion(y_pred, y_train)
+        loss.backward()
+        optimizer.step()
+        
+    # print training result
+    print(f'Epoch: {epoch+1:2} Loss: {loss.item():10.8f}')
+    
+print(f'\nDuration: {time.time() - start_time:.0f} seconds')
+
+# Predict into the future, plot
+
+window_size = 12
+future = 12
+L = len(y)
+
+preds = y_norm[-window_size:].tolist()
+
+model.eval()
+for i in range(future):  
+    seq = torch.FloatTensor(preds[-window_size:])
+    with torch.no_grad():
+        # Reset the hidden parameters here!
+        model.hidden = (torch.zeros(1,1,model.hidden_size),
+                        torch.zeros(1,1,model.hidden_size))  
+        preds.append(model(seq).item())
+
+# Inverse-normalize the prediction set
+true_predictions = scaler.inverse_transform(np.array(preds).reshape(-1, 1))
+
+# PLOT THE RESULT
+# Set a data range for the predicted data.
+# Remember that the stop date has to be later than the last predicted value.
+x = np.arange('2019-02-01', '2020-02-01', dtype = 'datetime64[M]').astype('datetime64[D]')
+
+plt.figure(figsize = (12,4))
+plt.title('Beer, Wine, and Alcohol Sales')
+plt.ylabel('Sales (millions of dollars)')
+plt.grid(True)
+plt.autoscale(axis = 'x',tight = True)
+plt.plot(df['S4248SM144NCEN'])
+plt.plot(x,true_predictions[window_size:])
 plt.show()
