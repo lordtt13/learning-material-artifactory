b01a06030bd17c4c431023470f1ffaad091eafb9
lordtt13 <thakurtanmay72@yahoo.com>
2020-05-05 07:10:20 +0530

    Dataloader on cuda add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- using_cuda.py
+++ using_cuda.py
@@ -67,3 +67,20 @@ next(model.parameters()).is_cuda
 gpumodel = model.cuda()
 
 next(gpumodel.parameters()).is_cuda
+
+# Load dataset
+
+df = pd.read_csv('../Data/iris.csv')
+X = df.drop('target',axis = 1).values
+y = df['target'].values
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+
+# Convert Tensors to .cuda() tensors
+
+X_train = torch.FloatTensor(X_train).cuda()
+X_test = torch.FloatTensor(X_test).cuda()
+y_train = torch.LongTensor(y_train).cuda()
+y_test = torch.LongTensor(y_test).cuda()
+
+trainloader = DataLoader(X_train, batch_size = 60, shuffle = True)
+testloader = DataLoader(X_test, batch_size = 60, shuffle = False)
