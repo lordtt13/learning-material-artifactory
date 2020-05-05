a46ae3ae0788f262ce6a8057df512f285a4c4b16
lordtt13 <thakurtanmay72@yahoo.com>
2020-05-05 07:03:50 +0530

    CUDA commands to get device properties add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

---
+++ using_cuda.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 07:02:06 2020
+
+@author: tanmay
+"""
+
+import torch
+
+
+torch.cuda.is_available()
+
+torch.cuda.current_device()
+
+torch.cuda.get_device_name(0)
+
+# Returns the current GPU memory usage by 
+# tensors in bytes for a given device
+torch.cuda.memory_allocated()
+
+# Returns the current GPU memory managed by the
+# caching allocator in bytes for a given device
+torch.cuda.memory_cached()
