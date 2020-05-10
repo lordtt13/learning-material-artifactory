01856ee4a2c0d3145a9bdcb01d013732f8af7fa5
lordtt13 <thakurtanmay72@yahoo.com>
2020-05-10 11:11:07 +0530

    One hot encoding func add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- NLP/nlp.py
+++ NLP/nlp.py
@@ -36,3 +36,26 @@ encoded_text = np.array([encoder[char] for char in text])
 encoded_text[:500]
 
 
+def one_hot_encoder(encoded_text, num_uni_chars):
+    '''
+    encoded_text : batch of encoded text
+    
+    num_uni_chars = number of unique characters (len(set(text)))
+    '''
+    
+    # Create a placeholder for zeros.
+    one_hot = np.zeros((encoded_text.size, num_uni_chars))
+    
+    # Convert data type for later use with pytorch (errors if we dont!)
+    one_hot = one_hot.astype(np.float32)
+
+    # Using fancy indexing fill in the 1s at the correct index locations
+    one_hot[np.arange(one_hot.shape[0]), encoded_text.flatten()] = 1.0
+    
+
+    # Reshape it so it matches the batch sahe
+    one_hot = one_hot.reshape((*encoded_text.shape, num_uni_chars))
+    
+    return one_hot
+
+one_hot_encoder(np.array([1,2,0]),3)
